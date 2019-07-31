257128a569d6f4e08353072be9c085372486baea
Liudmila Lysenko <llysenko23@rambler.ru>
2019-07-31 18:52:32 +0300

    feat(*): encrypt each value by separate crypto key

--- public/javascripts/decrypt.js
+++ public/javascripts/decrypt.js
@@ -5,20 +5,25 @@ module.exports.decrypt = async function () {
     const MODE_ENV = process.argv[2].split('=').splice(1, 2)[0];
     const fs = require('fs');
     const encFilesDir = './credentials-enc/';
+    let CRYPTO_KEY_ID;
+    let keys;
+    let obj;
 
-    fs.readdir(encFilesDir, (error, files) => {
-        if (error) throw error;
-        files.forEach(async (ciphertextFileName) => {
-            const envsFromFile = ciphertextFileName.split('.');
-            const modeFromFile = envsFromFile[0];
-            const nodeFromFile = envsFromFile[1];
+    fs.readFile(`./credentials/${MODE_ENV}.${NODE_ENV}.txt`, 'utf8', (err, data) => {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        keys = Object.keys(obj);
+        keys.forEach((key) => {
+            CRYPTO_KEY_ID = `${key}-key`;
+        });
 
-            if (nodeFromFile === NODE_ENV && modeFromFile === MODE_ENV) {
+        try {
+            const lineReader = require('readline').createInterface({
+                input: require('fs').createReadStream(`${encFilesDir}/${MODE_ENV}.${NODE_ENV}.txt.encrypted`)
+            });
+
+            lineReader.on('line', async (line) => {
                 const CRYPTO_KEY_RING_ID = `${MODE_ENV}-key-ring`;
-                const CRYPTO_KEY_ID = `${MODE_ENV}-${NODE_ENV}-key`;
-                const { promisify } = require('util');
-                const readFile = promisify(fs.readFile);
-                const contentsBuffer = await readFile(`credentials-enc/${ciphertextFileName}`);
                 const kms = require('@google-cloud/kms');
                 const client = new kms.KeyManagementServiceClient();
                 const name = client.cryptoKeyPath(
@@ -27,9 +32,8 @@ module.exports.decrypt = async function () {
                     CRYPTO_KEY_RING_ID,
                     CRYPTO_KEY_ID
                 );
-                const ciphertext = contentsBuffer.toString('base64');
-                const [result] = await client.decrypt({ name, ciphertext });
 
+                const [result] = await client.decrypt({ name, line });
                 // Writes the decrypted file to disk
                 const decrFilesDir = 'credentials';
 
@@ -37,13 +41,22 @@ module.exports.decrypt = async function () {
                     fs.mkdirSync(decrFilesDir);
                 }
 
+                const { promisify } = require('util');
                 const writeFile = promisify(fs.writeFile);
                 const plaintextFileName = `${MODE_ENV}.${NODE_ENV}.txt`;
                 await writeFile(`${decrFilesDir}/${plaintextFileName}`, Buffer.from(result.plaintext, 'base64'))
                     .catch(error => console.error(error));
 
-                console.info(`Decrypted ${ciphertextFileName}, result saved to ${plaintextFileName}.`);
-            }
-        })
+                console.info(`Decrypted ${MODE_ENV}.${NODE_ENV}.txt.encrypted, result saved to ${plaintextFileName}.`);
+            });
+
+        } catch (error) {
+            console.error(error);
+        }
     });
+
+    fs.readFile(`${encFilesDir}/${MODE_ENV}.${NODE_ENV}.txt.encrypted`, 'utf8', (err, data) => {
+        const arr = data.split(',');
+        console.log(arr)
+    })
 };
--- public/javascripts/encrypt.js
+++ public/javascripts/encrypt.js
@@ -5,46 +5,57 @@ module.exports.encrypt = async function () {
     const MODE_ENV = process.argv[2].split('=').splice(1, 2)[0];
     const decrFilesDir = './credentials/';
     const fs = require('fs');
-    fs.readdir(decrFilesDir, (error, files) => {
-        if (error) throw error;
-        files.forEach(async (file) => {
-            const envsFromFile = file.split('.');
-            const modeFromFile = envsFromFile[0];
-            const nodeFromFile = envsFromFile[1];
+    let CRYPTO_KEY_ID;
+    let keys;
+    let name;
+    let obj;
 
-            if (nodeFromFile === NODE_ENV && modeFromFile === MODE_ENV) {
-                const CRYPTO_KEY_RING_ID = `${MODE_ENV}-key-ring`;
-                const CRYPTO_KEY_ID = `${MODE_ENV}-${NODE_ENV}-key`;
-                const kms = require('@google-cloud/kms');
-                const client = new kms.KeyManagementServiceClient();
-                const name = client.cryptoKeyPath(
-                    process.env.GOOGLE_CLOUD_OAUTH_PROJECT_ID,
-                    process.env.CRYPTO_KEY_LOCATION,
-                    CRYPTO_KEY_RING_ID,
-                    CRYPTO_KEY_ID
-                );
+    fs.readFile(`${decrFilesDir}/${MODE_ENV}.${NODE_ENV}.txt`, 'utf8', (err, data) => {
+        const kms = require('@google-cloud/kms');
+        const client = new kms.KeyManagementServiceClient();
+        if (err) throw err;
+        obj = JSON.parse(data);
+        keys = Object.keys(obj);
+        keys.forEach((key) => {
+            CRYPTO_KEY_ID = `${key}-key`;
+            const CRYPTO_KEY_RING_ID = `${MODE_ENV}-key-ring`;
+            name = client.cryptoKeyPath(
+                process.env.GOOGLE_CLOUD_OAUTH_PROJECT_ID,
+                process.env.CRYPTO_KEY_LOCATION,
+                CRYPTO_KEY_RING_ID,
+                CRYPTO_KEY_ID
+            );
+            try {
+                const lineReader = require('readline').createInterface({
+                    input: require('fs').createReadStream(`${decrFilesDir}/${MODE_ENV}.${NODE_ENV}.txt`)
+                });
 
-                try {
-                    const { promisify } = require('util');
-                    const readFile = promisify(fs.readFile);
-                    const contentsBuffer = await readFile(`${decrFilesDir}/${file}`);
-                    const plaintext = contentsBuffer.toString('base64');
-                    const [result] = await client.encrypt({ name, plaintext });
-                    const writeFile = promisify(fs.writeFile);
+                lineReader.on('line', async (line) => {
+                    if (line.includes('}') || line.includes('{')) {
+                        return;
+                    }
+
+                    const [result] = await client.encrypt({ name, line });
                     const encFilesDir = 'credentials-enc';
 
                     if (!fs.existsSync(encFilesDir)) {
                         fs.mkdirSync(encFilesDir);
                     }
 
-                    const ciphertextFileName = `${file}.encrypted`;
-                    await writeFile(`${encFilesDir}/${ciphertextFileName}`, Buffer.from(result.ciphertext, 'base64'))
-                        .catch(error => console.error(error));
-                    console.info(`File ${file} encrypted and saved to ${ciphertextFileName}.`);
-                } catch (error) {
-                    console.error(error);
-                }
+                    const ciphertextFileName = `${MODE_ENV}.${NODE_ENV}.txt.encrypted`;
+                    const { promisify } = require('util');
+                    const appendFile = promisify(fs.appendFile);
+                    const data = Buffer.from(result.ciphertext, 'base64');
+
+                    await appendFile(`${encFilesDir}/${ciphertextFileName}`, `${CRYPTO_KEY_ID}: ${data},`);
+                    console.info(`Line ${line} encrypted and saved to ${ciphertextFileName}.`);
+                });
+
+            } catch (error) {
+                console.error(error);
             }
         });
+
+
     });
 };
